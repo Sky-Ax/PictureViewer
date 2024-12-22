ff73b48770f41552e77cda7050ea41dd8aa0823e
Sky-Ax <2939906374@qq.com>
2024-12-22 15:40:03 +0800

    feat: 暗黑模式/自定义主题色

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 
-const a = 10;
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <div>
+      <el-card style="max-width: 480px">
+        <template #header>
+          <div class="card-header">
+            <span>Card name</span>
+          </div>
+        </template>
+        <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+        <template #footer>Footer content</template>
+      </el-card>
+    </div>
   </main>
 </template>
